6dcd4d063a31399fa41ef491e64ef7eb4e5385d7
tanuj <tanuj@192.168.1.38>
2024-06-08 15:45:55 +0530

    change in print statemnet

--- javafirst/src/com/iquest/basics/FirstProgram.java
+++ javafirst/src/com/iquest/basics/FirstProgram.java
@@ -3,7 +3,7 @@ package com.iquest.basics;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,universe");
+		System.out.println("Hello,home");
 
 	}
 
