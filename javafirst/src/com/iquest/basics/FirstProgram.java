e7f5d43192db131243446b4c4b16a9486d8157dd
tanuj <tanuj@192.168.1.38>
2024-06-08 14:25:37 +0530

    change in the program.

--- javafirst/src/com/iquest/basics/FirstProgram.java
+++ javafirst/src/com/iquest/basics/FirstProgram.java
@@ -3,7 +3,7 @@ package com.iquest.basics;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,world");
+		System.out.println("Hello,universe");
 
 	}
 
